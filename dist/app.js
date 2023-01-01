735b8094efbc904ddca4dcd27cf35d43f94f4eb4
Jean Carloc Palomino Gonzales <jeanca2198@gmail.com>
2023-01-01 17:33:58 -0500

    Delete dist directory

--- dist/app.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_server_1 = __importDefault(require("./models/express-server"));
-const expressServer = new express_server_1.default();
-expressServer.listen();
-//# sourceMappingURL=app.js.map
